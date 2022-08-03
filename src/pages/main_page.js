613913fb9c242eb0ddfc37f0aa76a0802f0139a5
Nick Dziubynskyi <nick.dzub@gmail.com>
2022-08-03 20:00:20 +0300

    add: about comp

--- src/pages/main_page.js
+++ src/pages/main_page.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Banner from "../components/banner/banner";
+import About from "../components/about/about";
 
 class MainPage extends React.Component {
   render() {
     return (
       <>
         <Banner />
+        <About />
       </>
     );
   }
